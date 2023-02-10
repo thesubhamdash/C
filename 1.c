d28aa2a8533513f4472043f99be0cbba3f5b9ae1
MrDash <dassubham919@gmail.com>
2023-02-10 23:13:39 +0545

    c done

---
+++ 1.c
@@ -0,0 +1,15 @@
+// WAP to print sum of 1,2,3,4,...100 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,sum;
+  float x,y;
+  printf("Enter two integers");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("the sum is %d", sum);
+  getch();
+}
+
+
