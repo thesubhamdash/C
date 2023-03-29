012f0603a1e691b7a00e384df0a31784437731ec
MrDash <dassubham919@gmail.com>
2023-03-29 09:57:18 +0545

    done

--- 1.c
+++ 1.c
@@ -1,14 +1,16 @@
-// WAP to print sum of 1,2,3,4,...100 
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int a,b,sum;
-  float x,y;
-  printf("Enter two integers");
-  scanf("%d%d",&a,&b);
-  sum=a+b;
-  printf("the sum is %d", sum);
+  printf("Lets learn about pointer\n");
+  int a=69;
+  int* b=&a;
+  int* a1=NULL;
+  printf("The address of a is %p\n",&a);
+  printf("The address of a is %p\n",b);
+  printf("The address of b is %p\n",&b);
+  printf("The value of a is %d\n",*b);
+  printf("The garbage value of a1 is %p",a1);
   getch();
 }
 
