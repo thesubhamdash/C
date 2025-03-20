939ac89d7b96387b83c124b5425917c85d259805
Subham Das <dassubham919@gmail.com>
2025-03-20 15:47:42 +0530

    Task changed

--- C APNA COLLEGE/Lab Assignments/Temp.c
+++ C APNA COLLEGE/Lab Assignments/Temp.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 
 int main(){
-    float Fah=0,Cel;
-    printf("Enter a temp in Cecius");
-    scanf("%f", &Cel);
+    float Fah,Cel;
+    int a;
+    printf("Enter Temperaturre in Celcius(1) or Fahrenheit(2)?: ");
+    scanf("%d", &a);
+    switch (a){
+        case 1:
+            printf("Enter a temp in Cecius: ");
+            scanf("%f", &Cel);
+            Fah = ((9*Cel)/5)+32;
+            printf("Temp in Fahrenheit is: %f\n", Fah);
+            break;
+    
+        case 2:
+            printf("Enter a temp in Fahrenheit: ");
+            scanf("%f", &Fah);
+            Cel = (5*(Fah-32))/9;
+            printf("Temp in Celcius is: %f\n", Cel);
+            break;
 
-    Fah = ((9*Cel)/5)+32;
-    printf("Temp in Fahrenheit is: %f\n", Fah);
-
-    Cel = (5*(Fah-32))/9;
-    printf("Temp in Celcius is: %f\n", Cel);
+        default:
+            printf("Invalid");
+            break;
+    }
     return 0;
 }
