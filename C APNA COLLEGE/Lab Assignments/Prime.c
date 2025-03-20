d093d41cb12b967a446c0a1b40381d016af65017
Subham Das <dassubham919@gmail.com>
2025-03-20 14:04:44 +0530

    Prime

---
+++ C APNA COLLEGE/Lab Assignments/Prime.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int n,i,a=0;
+    printf("Enter a number:");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++){
+        if (n%i==0)
+        a++;
+    }
+    if (a==2){
+        printf("%d is a prime number", n);
+    }
+    else{
+        printf("Not Prime");
+    }
+    return 0;
+}
