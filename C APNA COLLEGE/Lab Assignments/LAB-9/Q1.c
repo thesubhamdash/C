ec8aa1f8599b0c592d017fc9d5b9fa6a56e5162f
Subham Das <dassubham919@gmail.com>
2025-03-27 13:45:45 +0530

    Changes in FH

--- C APNA COLLEGE/Lab Assignments/LAB-9/Q1.c
+++ C APNA COLLEGE/Lab Assignments/LAB-9/Q1.c
@@ -3,13 +3,13 @@
 int main()
 {
 FILE*fptr;
-fptr=fopen("sample.txt","w");	// for append mode-> "a"
+fptr=fopen("sample.txt","a");	// for append mode-> "a"
 if(fptr==NULL)
 	printf("No file");
 else
 	printf("File opened");
 
-fprintf(fptr,"Subham Kumar Das");		// for writing in the file.
+fprintf(fptr,"\nSubham Kumar Das");		// for writing in the file.
 fclose(fptr);
 return 0;
 }
