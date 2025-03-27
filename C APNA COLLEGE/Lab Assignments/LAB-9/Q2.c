ee4fc5f91dcae1cfd9b5b5e32e087ecf4e68940a
Subham Das <dassubham919@gmail.com>
2025-03-27 14:08:44 +0530

    Q2 needs to be recorrected

--- C APNA COLLEGE/Lab Assignments/LAB-9/Q2.c
+++ C APNA COLLEGE/Lab Assignments/LAB-9/Q2.c
@@ -2,14 +2,18 @@
 #include<stdlib.h>
 int main()
 {
-FILE*fptr;
-fptr=fopen("sample.txt","a");	// for append mode-> "a"
-if(fptr==NULL)
-	printf("No file");
-else
-	printf("File opened");
-
-fprintf(fptr,"\nSubham Kumar Das");		// for writing in the file.
-fclose(fptr);
-return 0;
+    FILE*fptr;
+    char text[1000];
+    fptr=fopen("sample.txt","r");	// for append mode-> "a"
+    if(fptr==NULL)
+	    printf("No file");
+    else
+	    printf("File opened");
+    while (!feof(fptr))
+    {
+        fgets(text,1000,fptr);
+    }
+    
+    fclose(fptr);
+    return 0;
 }
