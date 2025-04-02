21b87d07db674d71acc90d8b356c40e9e09f5553
Subham Das <dassubham919@gmail.com>
2025-04-02 22:38:24 +0530

    palindrome

---
+++ C APNA COLLEGE/Lab Assignments/LAB-2/Q1.c
@@ -0,0 +1,23 @@
+// WAP to check palindrome number:
+#include<stdio.h>
+int main(){
+    int n,a,sum=0;
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+    int num = n;
+    while (n!=0)
+    {
+        a = n % 10;
+        sum = sum*10 + a;
+        n /= 10;
+    }
+    if (num == sum)
+    {
+        printf("%d is palindrome\n", num);
+    }
+    else
+    {
+        printf("%d is not palindrome\n", num);
+    }
+    return 0;
+}
