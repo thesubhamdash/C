02ae128a4c48acadd9adeb5e0771f013011fcdca
Subham Das <dassubham919@gmail.com>
2025-09-07 10:36:35 +0530

    factorial using recursion

---
+++ C APNA COLLEGE/5-Functions & Recursions/Recursions/factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int fact(int n);
+
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("%d", fact(n));
+}
+
+int fact(int n){
+    int factorial=1;
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    factorial *= fact(n-1) * n;
+    return factorial;
+}
